b413d9bd85dadc4374d4320995a0d3fe6800a1b7
Ilya Cherniltcev <cherniltcev@mail.ru>
2025-02-08 00:02:51 +0500

    added case of all day user

--- src/main/java/org/example/StatisticReport.java
+++ src/main/java/org/example/StatisticReport.java
@@ -5,18 +5,34 @@ import java.util.stream.Collectors;
 
 public class StatisticReport {
     public HashMap<Long, Integer> getChampions(List<HashMap<Long, Integer>> daysInfo) {
+        // ---- filtering list by each user per each day
+        HashMap<Long, Integer> firstDay = daysInfo.get(0);
+        List<Long> usersIdFirstDay = firstDay.keySet().stream().toList();
+        List<HashMap<Long, Integer>> daysInfoCopy = new ArrayList<>();
+        for (int dayNumber = 0; dayNumber < daysInfo.size(); dayNumber++) {
+            // check each day by users list
+            for (Long userId : usersIdFirstDay) {
+                // if user exist then add this user to copy list
+                if (daysInfo.get(dayNumber).containsKey(userId)) {
+                    HashMap<Long, Integer> tempMap = new HashMap<>();
+                    tempMap.put(userId, daysInfo.get(dayNumber).get(userId));
+                    daysInfoCopy.add(dayNumber, tempMap);
+                }
+            }
+        }
+
         HashMap<Long, Integer> summaryInfo = new HashMap<>();
-        for (HashMap<Long, Integer> day : daysInfo) {
+        for (var day : daysInfoCopy) {
             for (Long userId : day.keySet()) {
                 int tempStep = summaryInfo.getOrDefault(userId, 0);
                 summaryInfo.put(userId, day.getOrDefault(userId, 0) + tempStep);
             }
         }
         // ----- find maximum element
-             int maxSteps = summaryInfo.entrySet()
-                    .stream()
-                    .max(Comparator.comparing(Map.Entry::getValue))
-                     .orElseThrow().getValue();
+        int maxSteps = summaryInfo.entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .orElseThrow().getValue();
 
         // ----- filtering map by maximum value and make new Map
         return (HashMap<Long, Integer>) summaryInfo.entrySet().stream()
--- src/test/java/StatisticReportTest.java
+++ src/test/java/StatisticReportTest.java
@@ -8,18 +8,21 @@ import java.util.*;
 public class StatisticReportTest {
     StatisticReport report = new StatisticReport();
     List<HashMap<Long, Integer>> daysList = new ArrayList<>();
-    HashMap<Long, Integer> day1Info = new HashMap<>();;
+    HashMap<Long, Integer> day1Info = new HashMap<>();
+    ;
     HashMap<Long, Integer> day2Info = new HashMap<>();
 
     HashMap<Long, Integer> resultMapFirstCase = new HashMap<>(Map.of(2L, 2500));
     HashMap<Long, Integer> resultMapSecondCase = new HashMap<>(Map.of(1L, 5500, 2L, 5500));
+    HashMap<Long, Integer> resultMapThirdCase = new HashMap<>(Map.of(1L, 3000));
 
     @AfterEach
-    void reset(){
+    void reset() {
         day1Info.clear();
         day2Info.clear();
         daysList.clear();
     }
+
     @Test
     void getChampionsTestFirstCase() {
         day1Info.put(1L, 1000);
@@ -48,4 +51,18 @@ public class StatisticReportTest {
         Assertions.assertEquals(resultMapSecondCase, actual);
     }
 
+
+    @Test
+    void getChampionsTestIfUsersStepsMaxOnlyInOneDay() {
+        day1Info.put(1L, 1000);
+
+        day2Info.put(2L, 6000);
+        day2Info.put(1L, 2000);
+
+        daysList.add(day1Info);
+        daysList.add(day2Info);
+
+        HashMap<Long, Integer> actual = report.getChampions(daysList);
+        Assertions.assertEquals(resultMapThirdCase, actual);
+    }
 }
